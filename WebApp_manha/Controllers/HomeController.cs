d25e49c2ae6d6df46b6c00d3a2f2f0b63d4126ae
0220832211035 <0220832211035@LAB4MAQ03>
2023-03-22 08:52:42 -0300

    Add project files.

---
+++ WebApp_manha/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_manha.Models;
+
+namespace WebApp_manha.Controllers
+{
+    public class HomeController : Controller
+    {   //atributo da classe HomeController
+        private readonly ILogger<HomeController> _logger;
+        //Construtor de Classe
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Action Result devolve uma tela (HTML)
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Mais uma tela no sistema
+        public IActionResult BemVindo()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
